b3395392c6c1a78ca09fe06e0dcda2478fae4c6b
jannydream826 <ssenzo@udata.co.jp>
2023-09-11 11:41:55 +0200

    feat: add signin component to signin page

---
+++ src/pages/SignInPage.tsx
@@ -0,0 +1,13 @@
+import SignInForm from "../components/signin/SignInForm";
+
+const SignInPage = () => {
+    return (
+        <div className="flex justify-between">
+            <div className="mt-[271px] ml-[308px]">
+                <SignInForm />
+            </div>
+        </div>
+    )
+}
+
+export default SignInPage;
