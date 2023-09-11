1cee3ea2b781e836b7b5a80b2419cc7a17627e72
jannydream826 <ssenzo@udata.co.jp>
2023-09-11 06:30:31 +0200

    chore: add flowbit dependencies and config

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import 'flowbite';
 // import './assets/tailwindcss.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
