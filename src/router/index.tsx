4f8e1194316f71dd030e0e9b237771eafb0afa85
jannydream826 <ssenzo@udata.co.jp>
2023-09-11 08:13:22 +0200

    feat: define routes for signin page and homepage

---
+++ src/router/index.tsx
@@ -0,0 +1,39 @@
+import { lazy, Suspense } from "react";
+import type { RouteObject } from 'react-router-dom';
+import Layout from "../layout/Layout";
+
+const Loadable =
+  (Component: React.ComponentType<any>) => (props: JSX.IntrinsicAttributes) =>
+    (
+      <Suspense fallback={<p>loading</p>}>
+        <Component {...props} />
+      </Suspense>
+    );
+
+const SignInPage = Loadable(lazy(() => import('../pages/SignIn')));
+const HomePage = Loadable(lazy(() => import('../pages/HomePage')));
+
+const authRoutes: RouteObject = {
+    path: '*',
+    children: [
+      {
+        path: 'signin',
+        element: <SignInPage />,
+      }
+    ],
+};
+
+const normalRoutes: RouteObject = {
+    path: '*',
+    element: <Layout />,
+    children: [
+      {
+        index: true,
+        element: <HomePage />,
+      }
+    ],
+};
+
+const routes: RouteObject[] = [authRoutes, normalRoutes];
+
+export default routes;
